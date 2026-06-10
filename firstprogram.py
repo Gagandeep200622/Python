fef01428bef2ce01f8e85c95d0ad578b46887892
Gagandeep R Naik <gagannaik8050@gmail.com>
2026-06-10 12:33:48 +0530

    Add files via upload

---
+++ firstprogram.py
@@ -0,0 +1,60 @@
+a = 10
+b = 20
+sum = a + b
+
+print(sum) #a+b=30
+
+#Arithematic Operators
+a = 5
+b = 10
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a ** b)
+print(a % b)
+
+#relational/comparison operators
+a = 50
+b = 30
+ 
+print(a == b) #False
+print(a != b) #True
+print(a > b) #True
+print(a < b) #False
+print(a >= b) #True
+print(a <= b) #False
+
+#assignment operator
+num = 10
+num += 10
+print (num)
+
+num -= 10
+print (num)
+
+num *= 10
+print(num)
+
+num /= 10
+print(num)
+
+num %= 10
+print(sum)
+
+num **= 10
+print (num)
+ 
+#logical operators
+a = 50
+b = 30
+
+print(not True)
+print(not (a>b)) 
+
+
+val1 = False
+val2 = True
+print("and operator:",val1 and val2)
+print("OR operator:",a==b or a>b)
