cd9f646bd5297db7370c46f59f96c4e1246e297a
Gagandeep R Naik <gagannaik8050@gmail.com>
2026-06-11 11:06:53 +0530

    Add files via upload

---
+++ 2ndprogram.py
@@ -0,0 +1,34 @@
+#Variables are used to store data in a program
+
+name = "gagan"
+age = 20
+height = 5.8
+
+print(name)
+print(age)
+print(height)
+
+#data types
+
+name = "Gagan"
+age = 20
+height = 6.0
+is_student = True
+print(type(name))          #sring
+print(type(age))           #integer 
+print(type(height))        #float       
+print(type(is_student))    #boolean
+
+#differnt data types
+
+a = 10 
+b = 10.0
+c = "python"
+d = True 
+
+print (type(a))
+print (type(b))
+print (type(c))
+print (type(d))
+
+
