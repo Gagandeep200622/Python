441b58f1322fb52024e6a578b359deecec0b8675
Gagandeep R Naik <gagannaik8050@gmail.com>
2026-06-11 11:07:22 +0530

    Add files via upload

---
+++ 3rdprogram.py
@@ -0,0 +1,68 @@
+# #type conversion
+# a = 2
+# b = 4.25
+# sum = a + b
+# print (sum)
+
+# #type casting
+# a = int("2")
+# b = 4.35
+# print(type(a))
+# print(a + b)
+
+# a= 3.36
+# a = str(a)
+# print(type(a))
+
+
+#input in python
+# name = input("enter your name :")
+# print("welcome",name)
+
+# age = int(input("enter your age : "))
+# print("you enterd ", (age))
+
+
+# val = input("enter some value: ") #result for input() is always a str
+# print(type(val),val)
+
+
+# val = int(input("enter som value:  "))
+# print(type(val),val)
+# val = float(input("enter some value:  "))
+# print(type(val),val)
+
+
+# name = input("enter your name: ")
+# age = int(input("enter your age :"))
+# marks =float(input("enter your marks : "))
+
+# print("welcome",name)
+# print("age= ", age)
+# print("maeks = " ,marks)
+
+
+#write a program to input 2 numbers and print their sum.
+# num1 = int(input("first number: "))
+# num2 = int(input("second number: "))
+# sum = (num1 + num2)
+
+# print("sum =",num1 + num2)
+
+
+#write a program to input side of a sqare its area
+
+# side = int(input("enter sqare side : "))
+
+# print("area =",side ** 2 )
+
+# write a program to input 2 floating point and print their average
+# point1 = float(input("point number one: "))
+# point2 = float(input("point number two : "))
+# print("avg = ",(point1 + point2 )/2)
+
+# Write a program to input 2 int numbers a and b .Print True if agreater than or equal to b . If not eqal print False
+
+a = int (input("enter first number : "))
+b = int (input(" enter second number : "))
+print(a>=b)
