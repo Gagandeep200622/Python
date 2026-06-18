e894e435cb5ca9d8e086bc24ca7a273b4b944750
Gagandeep R Naik <gagannaik8050@gmail.com>
2026-06-18 20:55:17 +0530

    Add files via upload

---
+++ python9.py
@@ -0,0 +1,42 @@
+# functions in python 
+
+
+def cal_sum(a, b):  #function defination
+    sum = a + b #parameters 
+    print(sum)
+    return sum
+
+cal_sum(5, 9) # function call; argument
+
+cal_sum(2 , 6) 
+
+
+def sum(a, b):
+    val = a + b
+    print(val)
+    return val
+
+sum(1, 3)
+
+
+
+def print_hello():
+    print("hello")
+
+print_hello()
+print_hello()
+
+
+def calc_avg(a, b, c):
+    sum = a + b + c
+    avg = sum / 3
+
+    print(avg)
+    return avg
+
+calc_avg(3, 5, 6, )
+calc_avg(4, 7, 9, )
+   
+
+
+
