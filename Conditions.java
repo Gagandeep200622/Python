5f07c52bec5feca7d4ee27d4c3a6991415a3950b
Gagandeep R Naik <gagannaik8050@gmail.com>
2026-09-22 21:49:47 +0530

    Add files via upload

---
+++ Conditions.java
@@ -0,0 +1,20 @@
+// take the age input from user and check he is an adult or not an adult
+
+import java.util.*;
+
+public class Conditions {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if (age>=18){
+            System.out.println("Adult");
+        }
+            else{
+                System.out.println("not an adlut");
+            }
+        }
+    
+        
+    }
+
+
