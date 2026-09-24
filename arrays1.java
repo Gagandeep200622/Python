dbcacee2b91a7f436598130c6447fd633131bcb6
Gagandeep R Naik <gagannaik8050@gmail.com>
2026-09-24 20:57:58 +0530

    Add files via upload

---
+++ arrays1.java
@@ -0,0 +1,14 @@
+//print the marks of 3 subjects of the student
+
+public class arrays1{
+    public static void main(String[] args){
+        int[] marks = new int[3];
+        marks[0] = 99;
+        marks[1] = 96;
+        marks[2] = 88;
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+
+    }
+}
