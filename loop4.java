ebccb64f24057d8b14af262e0bd918b78bce5e82
Gagandeep R Naik <gagannaik8050@gmail.com>
2026-09-26 18:18:39 +0530

    Add files via upload

---
+++ loop4.java
@@ -0,0 +1,13 @@
+// print the table of a number given by the user 
+
+import java.util.*;
+public class loop4{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        for(int i = 1; i < 11; i++){
+            System.out.println(n * i);
+        
+        }
+    }
+}
